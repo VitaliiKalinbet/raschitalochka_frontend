0c2282df3a20fec7443f2b29b20c813f95931cb8
AlxndrYatsenko <oldr.yatsenko@gmail.com>
2019-03-26 16:12:25 +0200

    added responsive and images

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,8 +6,18 @@ import s from './Login.module.css';
 class Login extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    width: 800
   };
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions.bind(this));
+  }
 
   handleSubmit = e => {
     e.preventDefault();
@@ -23,39 +33,54 @@ class Login extends Component {
     });
   };
 
+  updateDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
   render() {
-    const { email, password } = this.state;
+    const { email, password, width } = this.state;
+    console.log(width);
     return (
-      <div className={s.formWrapper}>
-        <form className={s.form} onSubmit={this.handleSubmit}>
-          <h3 className={s.formTitle}>Raschitalochka</h3>
-          <input
-            className={s.input}
-            name="email"
-            placeholder="E-mail as Login"
-            type="email"
-            value={email}
-            required
-            autoComplete="email"
-            onChange={this.handleChange}
-          />
-          <input
-            className={s.input}
-            name="password"
-            placeholder="Password"
-            type="password"
-            value={password}
-            required
-            autoComplete="new-password"
-            onChange={this.handleChange}
-          />
-          <button className={s.submitBtn} type="submit">
-            Enter
-          </button>
-          <NavLink className={s.registerLink} activeClassName={s.activeLink} to="/registration">
-            Register
-          </NavLink>
-        </form>
+      <div className={s.wrap}>
+        <div className={s.bgWrap}>
+          {width >= 768 && <p className={s.slogan}>Manage your budget with finance app</p>}
+        </div>
+        <div className={s.formWrapper}>
+          <form className={s.form} onSubmit={this.handleSubmit}>
+            <h3 className={s.formTitle}>Raschitalochka</h3>
+            <div className={s.inputWithIcon}>
+              <input
+                className={s.inputEmail}
+                name="email"
+                placeholder="E-mail as Login"
+                type="email"
+                value={email}
+                required
+                onChange={this.handleChange}
+              />
+              <i className={s.iconEmail} />
+            </div>
+            <div className={s.inputWithIcon}>
+              <input
+                className={s.inputPass}
+                name="password"
+                placeholder="Password"
+                type="password"
+                value={password}
+                required
+                onChange={this.handleChange}
+              />
+              <i className={s.iconPass} />
+            </div>
+
+            <button className={s.submitBtn} type="submit">
+              Enter
+            </button>
+            <NavLink className={s.registerLink} activeClassName={s.activeLink} to="/registration">
+              Register
+            </NavLink>
+          </form>
+        </div>
       </div>
     );
   }
